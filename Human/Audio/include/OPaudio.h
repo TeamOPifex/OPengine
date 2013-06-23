a13d84d365f3d695f6391a79a0c21527a8bc471b
mrpossoms <none@none>
2013-06-23 15:47:37 -0400

    --HG-- branch : Audio

--- Human/Audio/include/OPaudio.h
+++ Human/Audio/include/OPaudio.h
@@ -7,7 +7,7 @@
 #include "Data/include/OPfile.h"
 #include "Human/Math/Vector3.h"
 
-#include "./Core/include/Log.h"
+#include "./Core/include/Log.h"5
 
 #ifdef OPIFEX_ANDROID
 #include <SLES/OpenSLES.h>
