fb2f8d633811a6e6de1f24b9d85973f06504c672
mrpossoms <none@none>
2013-07-24 16:28:00 -0400

    Stuff
    --HG--
    branch : KirksAudio

--- Human/Audio/include/OPaudio2.h
+++ Human/Audio/include/OPaudio2.h
@@ -89,7 +89,7 @@ LPOVOPENCALLBACKS   fn_ov_open_callbacks;
 // | |  | |  ___/ _` | | | |/ _` | |/ _ \  |  __| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
 // | |__| | |  | (_| | |_| | (_| | | (_) | | |  | |_| | | | | (__| |_| | (_) | | | \__ \
 //  \____/|_|   \__,_|\__,_|\__,_|_|\___/  |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
-void OPint OPaudInit();
+OPint OPaudInit();
 
 Vector3 OPaudEarPosition(Vector3 pos);
 Vector3 OPaudEarVelocity(Vector3 pos);
