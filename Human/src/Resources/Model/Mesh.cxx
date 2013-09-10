35354a41737df6d4ced51747b093e1cf41bf4b9c
mrpossoms <none@none>
2013-09-10 12:21:25 -0400

    Temporarily removing the Resources directory
    --HG--
    branch : C rendering system

--- Human/src/Resources/Model/Mesh.cxx
+++
@@ -1,6 +0,0 @@
-#include "./Human/include/Resources/Model/Mesh.h"
-
-Mesh::Mesh(BufferPtr vb, BufferPtr ib, ui32 count, ui32 stride) : BaseMesh(count, stride) {
-	VertexBuffer = vb;
-	IndexBuffer = ib;
-}
