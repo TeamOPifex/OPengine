b45f56423c4673e8e09ecf3691337d783a6e6208
rentabuddha <none@none>
2013-09-11 21:15:06 -0400

    Backed out changeset: 317d840ec5ec
    --HG--
    branch : C rendering system

--- Human/include/Input/GamePadSystem.h
+++ Human/include/Input/GamePadSystem.h
@@ -19,15 +19,13 @@ typedef struct{
 
 extern GamePadController GamePadControllers[CONTROLLERS];
 
+
 GamePadController* OPgamePadController(GamePadIndex index);
 
 void OPgamePadUpdate(GamePadController* controller);
 void OPgamePadReset(GamePadController* controller);
 void OPgamePadSetDeadzone(GamePadController* controller, OPfloat deadzone);
 
-void OPgamePadSystemUpdate();
-void OPgamePadSystemReset();
-void OPgamePadSetDeadzones(OPfloat deadzone);
 
 bool OPgamePadIsDown(GamePadController* controller, GamePadButton button);
 bool OPgamePadIsUp(GamePadController* controller, GamePadButton button);	
