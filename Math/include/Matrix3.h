caf7e646b35ce28f95081138a905841aa96a480f
Garrett Hoofman <gambitsunob@gmail.com>
2013-09-18 20:55:46 -0400

    Inlines
    --HG--
    branch : Inline Math Functions

--- Math/include/Matrix3.h
+++ Math/include/Matrix3.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include "Vector3.h"
+#include "./Core/include/DynamicMemory.h"
 
 typedef struct {
 	OPvec3 cols[3];
 } OPmat3;
 
-inline void OPmat3identity(OPmat3* dst);
+inline void OPmat3identity(OPmat3* dst) {
+	OPbzero(dst, sizeof(OPmat3));
+	dst->cols[0].x = 1;
+	dst->cols[1].y = 1;
+	dst->cols[2].z = 1;
+}
--- Math/include/Vector4.h
+++ Math/include/Vector4.h
@@ -6,4 +6,10 @@ typedef struct {
 	OPfloat x, y, z, w;
 } OPvec4;
 
-inline void OPvec4norm(OPvec4* dst, OPvec4* a);
+inline void OPvec4norm(OPvec4* dst, OPvec4* a) {
+	OPfloat l = OPsqrt(a->x * a->x + a->y * a->y + a->z * a->z + a->w * a->w);
+	dst->x = a->x / l;
+	dst->y = a->x / l;
+	dst->z = a->x / l;
+	dst->w = a->x / l;
+}
