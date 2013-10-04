e109e0d030ea14c405829296e8f43d853c1c1c82
mrpossoms <none@none>
2013-10-04 01:09:09 -0400

    Inital commit of OPthread header
    --HG--
    branch : C rendering system

---
+++ Performance/include/OPthread.h
@@ -0,0 +1,38 @@
+#ifndef OPPERF_THREAD
+#define OPPERF_THREAD
+
+#include "Core/include/Types.h"
+
+#if defined(OPIFEX_LINUX32) || defined(OPIFEX_LINUX64) || defined(OPIFEX_ANDROID)
+#include <pthread.h>
+#elif defined(OPIFEX_WIN32) || defined(OPIFEX_WIN64)
+
+#endif
+
+//-----------------------------------------------------------------------------
+//   _____ _                   _       
+//  / ____| |                 | |      
+// | (___ | |_ _ __ _   _  ___| |_ ___ 
+//  \___ \| __| '__| | | |/ __| __/ __|
+//  ____) | |_| |  | |_| | (__| |_\__ \
+// |_____/ \__|_|   \__,_|\___|\__|___/
+typedef struct{
+#if defined(OPIFEX_LINUX32) || defined(OPIFEX_LINUX64) || defined(OPIFEX_ANDROID)
+	pthread_t Thread;
+#elif defined(OPIFEX_WIN32) || defined(OPIFEX_WIN64)
+
+#endif
+} OPthread;
+
+//-----------------------------------------------------------------------------
+// ______                _   _                 
+//|  ____|              | | (_)                
+//| |__ _   _ _ __   ___| |_ _  ___  _ __  ___ 
+//|  __| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+//| |  | |_| | | | | (__| |_| | (_) | | | \__ \
+//|_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+OPthread OPthreadStart(void* (*function) (void*));
+OPint    OPthreadStop(OPthread* thread);
+OPint    OPthreadJoin(OPthread* thread);
+
+#endif
