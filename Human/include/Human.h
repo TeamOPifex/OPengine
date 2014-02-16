947f588a994a83a53f4acb6a660a197cf20c1c4a
mrpossoms <none@none>
2014-02-16 17:55:13 -0500

    Forgot to add Human.h...

---
+++ Human/include/Human.h
@@ -0,0 +1,8 @@
+#ifndef OPENGINE_HUMAN
+#define OPENGINE_HUMAN
+#include "./Systems/AudioSystem.h"
+#include "./Systems/FontSystem.h"
+#include "./Systems/InputSystem.h"
+#include "./Systems/RenderSystem.h"
+#include "./Utilities/DefaultLoaders.h"
+#endif
