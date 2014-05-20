75e528a08b99428ec21b418d91284d50f6ef366e
mrpossoms <none@none>
2014-05-19 20:46:06 -0400

    Added hex dump
    --HG--
    branch : EntHeap restructure and testing
    
    
    Former-commit-id: 031894dd2b9c9d09f5beb6f7ff1a639a61769116

--- Tests/include/helpers.h
+++ Tests/include/helpers.h
@@ -35,4 +35,38 @@ OPint OP_TEST(OPint (*testRoutine)(void*), const OPchar* name, void* args){
 	return result;
 }
 
+void OP_HEX_DUMP(void *addr, OPint len) {
+    OPint i;
+    ui8 buff[17];
+    ui8 *pc = (ui8*)addr;
+	ui8 desc[1024];
+
+    // Output description if given.
+    if (desc != NULL)
+        printf ("%s:\n", desc);
+
+    // Process every byte in the data.
+    for (i = 0; i < len; i++) {
+        // Multiple of 16 means new line (with line offset).
+
+        if ((i % 16) == 0) {
+            // Just don't print ASCII for the zeroth line.
+            if (i != 0)
+                printf ("\t%s\n", buff);
+
+            // Output the offset.
+            printf ("  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+        printf (" %02x", pc[i]);
+
+        // And store a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+}
 #endif
