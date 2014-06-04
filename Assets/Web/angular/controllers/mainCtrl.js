dfc2a185bc8897b98b3c728b52be417fd0c2f6a8
Garrett Hoofman <gambitsunob@gmail.com>
2014-06-04 19:39:02 -0400

    Playing with accelerometer data

--- Assets/Web/angular/controllers/mainCtrl.js
+++ Assets/Web/angular/controllers/mainCtrl.js
@@ -44,4 +44,27 @@ angular.module('opengine')
                 break;
             }
         }
+
+        function tilt(x, y) {
+            if(x == null || y == null) return;
+            console.log(x, y);
+            $scope.fontx = y / 100.0;
+            $scope.fonty = x / 100.0;
+            $scope.$digest();
+        }
+
+        if (window.DeviceOrientationEvent) {
+            window.addEventListener("deviceorientation", function () {
+                tilt(event.beta, event.gamma);
+            }, true);
+        } else if (window.DeviceMotionEvent) {
+            window.addEventListener('devicemotion', function () {
+                tilt(event.acceleration.x * 2, event.acceleration.y * 2);
+            }, true);
+        } else {
+            window.addEventListener("MozOrientation", function () {
+                tilt(orientation.x * 50, orientation.y * 50);
+            }, true);
+        }
+
     }]);
