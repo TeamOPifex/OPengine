a097b45edd6b7bf6b15ce74f62792c7d3c1fef9a
Garrett Hoofman <gambitsunob@gmail.com>
2014-06-04 22:04:28 -0400

    Gyro fix if not available
    Former-commit-id: d872840033fabb08c025f0feebf587b87daefd6b

--- Assets/Web/js/angular/controllers/mainCtrl.js
+++ Assets/Web/js/angular/controllers/mainCtrl.js
@@ -49,11 +49,13 @@ angular.module('opengine')
         }
 
         gyro.frequency = 20;
-        gyro.startTracking(function(o) {
-            // o.x, o.y, o.z for accelerometer
-            // o.alpha, o.beta, o.gamma for gyro
-            $scope.fontx = o.alpha / 10.0;
-            $scope.fonty = o.beta / 10.0;
-            $scope.$digest();
-        });
+        if(gyro.getFeatures().length > 0) {
+            gyro.startTracking(function(o) {
+                // o.x, o.y, o.z for accelerometer
+                // o.alpha, o.beta, o.gamma for gyro
+                $scope.fontx = o.alpha / 10.0;
+                $scope.fonty = o.beta / 10.0;
+                $scope.$digest();
+            });
+        }
     }]);
