31944ae9b7f39be356fa0a64739ea344c1c123a7
Garrett Hoofman <gambitsunob@gmail.com>
2014-09-13 15:19:20 -0400

    GameState renamed methods
    Former-commit-id: 12af7a31b66a3f905497275e3533316890d646c1

--- Data/src/GameState.c
+++ Data/src/GameState.c
@@ -5,17 +5,17 @@ OPgameState* ActiveState = NULL;
 void OPgameStateChange(OPgameState* targetState){
 	OPgameState* lastState = ActiveState;
 	
-	if(ActiveState && ActiveState->OnExit != NULL) ActiveState->OnExit(targetState);
+	if (ActiveState && ActiveState->Destroy != NULL) ActiveState->Destroy(targetState);
 	ActiveState = targetState;
-	if(ActiveState->OnEntrance != NULL) ActiveState->OnEntrance(lastState);
+	if (ActiveState->Init != NULL) ActiveState->Init(lastState);
 }
 
 OPgameState* OPgameStateCreate(void (*entrance)(OPgameState*), int(*update)(OPtimer*), void (*exit)(OPgameState*)){
 	OPgameState* gs = (OPgameState*)OPalloc(sizeof(OPgameState));
 
-	gs->OnEntrance = entrance;
+	gs->Init = entrance;
 	gs->Update = update;
-	gs->OnExit = exit;
+	gs->Destroy = exit;
 
 	return gs;
 }
