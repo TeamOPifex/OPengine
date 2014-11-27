ec006167cbb4db17ce93860685543df62f4e2fee
Garrett Hoofman <gambitsunob@gmail.com>
2014-11-27 16:33:48 -0600

    doc test
    Former-commit-id: b55dbe0f9a58f02f8efa4298c5a7446301521a56
    Former-commit-id: d5201979ead0cf59b45cde0903f15bc5e5b931cd

--- Math/include/BoundingBox2D.h
+++ Math/include/BoundingBox2D.h
@@ -8,6 +8,11 @@ typedef struct {
 	OPvec2 max;
 } OPboundingBox2D;
 
+/* Creates an OPboundingBox2D
+ * @param min Minimum bounds of the bounding box to create
+ * @param max Maximum bounds of the bounding box to create
+ * @return the resulting box
+*/
 inline OPboundingBox2D OPboundingBox2DCreate(OPvec2 min, OPvec2 max) {
 	OPboundingBox2D tmp = {
 		min,
