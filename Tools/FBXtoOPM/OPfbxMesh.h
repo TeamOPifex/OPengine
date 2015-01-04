c4831b0f62b8cb3ec86f7ef3ac739afdd5bafda1
Garrett Hoofman <gambitsunob@gmail.com>
2015-01-04 18:10:08 -0500

    Cleaning up the FBXtoOPM

--- Tools/FBXtoOPM/OPfbxMesh.h
+++ Tools/FBXtoOPM/OPfbxMesh.h
@@ -15,15 +15,9 @@ typedef struct {
 	OPfbxSkeleton Skeleton;
 	OPfbxMeshData MeshData;
 
-	//ui32* Indices;
-	//OPvec3* Positions;
-	//OPvec2* UVs;
-	//OPvec3* Normals;
 	OPfbxMeshPoly* Polys;
 	OPfbxSkinBlendWeight* BlendWeights;
 	OPfbxAnimation* Animation;
-
-	//OPfbxSkin Skin;
 
 } OPfbxMesh;
 
@@ -45,17 +39,6 @@ OPint OPfbxMeshCreate(OPfbxMesh* mesh, const OPchar* filename) {
 
 		mesh->Polys = OPfbxMeshDataGetPolygons(&mesh->MeshData);
 
-		//mesh->Indices = OPfbxMeshDataGetIndices(&mesh->MeshData);
-		//OPlog("Step: indices");
-		//mesh->Positions = OPfbxMeshDataGetPositions(&mesh->MeshData);
-		//OPlog("Step: positions");
-		//mesh->UVs = OPfbxMeshDataGetUVs(&mesh->MeshData);
-		//OPlog("Step: uvs");
-		//mesh->Normals = OPfbxMeshDataGetNormals(&mesh->MeshData);
-		//OPlog("Step: normals");
-
-		//OPfbxSkinGet(&mesh->Skin, &mesh->MeshData, &mesh->Skeleton);
-
 		mesh->BlendWeights = _skinBlendWeights(&mesh->MeshData, &mesh->Skeleton, &mesh->SCENE);
 		OPlog("Step: skin");
 
@@ -73,163 +56,172 @@ OPint OPfbxMeshCreate(OPfbxMesh* mesh, const OPchar* filename) {
 	return 1;
 }
 
-OPint OPfbxMeshWriteToFile(OPfbxMesh* mesh, const OPchar* filename, OPint* features) {
+OPint OPfbxMeshWriteToFile(OPfbxMesh* mesh, const OPchar* filename, OPint* features, OPint animOnly) {
 	ofstream myFile(filename, ios::binary);
 	OPlog("Begin Writing File...");
 
-	// OPM File Format Version
-	writeU16(&myFile, 1);
+	if (!animOnly) {
+		// OPM File Format Version
+		writeU16(&myFile, 1);
 
-	OPlog("Feature[Model_Positions]: %d", features[Model_Positions]);
-	OPlog("Feature[Model_Normals]: %d", features[Model_Normals]);
-	OPlog("Feature[Model_UVs]: %d", features[Model_UVs]);
-	OPlog("Feature[Model_Colors]: %d", features[Model_Colors]);
-	OPlog("Feature[Model_Indices]: %d", features[Model_Indices]);
-	OPlog("Feature[Model_Bones]: %d", features[Model_Bones]);
-	OPlog("Feature[Model_Skinning]: %d", features[Model_Skinning]);
-	OPlog("Feature[Model_Animations]: %d", features[Model_Animations]);
+		OPlog("Feature[Model_Positions]: %d", features[Model_Positions]);
+		OPlog("Feature[Model_Normals]: %d", features[Model_Normals]);
+		OPlog("Feature[Model_UVs]: %d", features[Model_UVs]);
+		OPlog("Feature[Model_Colors]: %d", features[Model_Colors]);
+		OPlog("Feature[Model_Indices]: %d", features[Model_Indices]);
+		OPlog("Feature[Model_Bones]: %d", features[Model_Bones]);
+		OPlog("Feature[Model_Skinning]: %d", features[Model_Skinning]);
+		OPlog("Feature[Model_Animations]: %d", features[Model_Animations]);
 
-	ui32 feature = 0;
-	if (features[Model_Positions]) feature += 0x01;
-	if (features[Model_Normals]) feature += 0x02;
-	if (features[Model_UVs]) feature += 0x04;
-	if (features[Model_Colors]) feature += 0x100;
-	if (features[Model_Indices]) feature += 0x10;
-	if (features[Model_Bones]) feature += 0x20;
-	if (features[Model_Skinning]) feature += 0x40;
-	if (features[Model_Animations]) feature += 0x80;
-
-
-	OPlog("Feature: %d", feature);
-	// OPM File Features
-	writeU32(&myFile, feature);
-
-	OPint verticeCount = 0;
-	OPint indCount = 0;
-	for (OPint i = 0; i < mesh->MeshData.PolyCount; i++) {
-		verticeCount += mesh->Polys[i].Size;
-		indCount += mesh->Polys[i].Size == 3 ? 3 : 6;
-	}
-
-	OPlog("Vertex Count: %d", verticeCount);
-	// Vertex Count
-	writeU32(&myFile, verticeCount);
-
-	for (OPint i = 0; i < mesh->MeshData.PolyCount; i++) {
-		for (OPint j = 0; j < mesh->Polys[i].Size; j++) {
-
-			if (features[Model_Positions]) {
-				OPlg(".");
-				OPvec3 pos = mesh->Polys[i].Position[j];
-				writeF32(&myFile, pos.x);
-				writeF32(&myFile, pos.y);
-				writeF32(&myFile, pos.z);
-			}
-
-			if (features[Model_Normals]) {
-				OPlg("-");
-				OPvec3 norm = mesh->Polys[i].Normal[j];
-				writeF32(&myFile, norm.x);
-				writeF32(&myFile, norm.y);
-				writeF32(&myFile, norm.z);
-			}		
-			
-			if (features[Model_UVs]) {
-				OPlg("=");
-				OPvec2 uv = mesh->Polys[i].UV[j];
-				writeF32(&myFile, uv.x);
-				writeF32(&myFile, uv.y);
-			}
-
-			if (features[Model_Skinning]) {
-				OPlg("+");
-				OPint cp = mesh->Polys[i].ControlPointIndex[j];
-				OPfbxSkinBlendWeight bw = mesh->BlendWeights[cp];
-				OPint boneIndex;
-
-				if (bw.c > 0) {
-					boneIndex = OPfbxSkeletonGetPos(&mesh->Skeleton, mesh->BlendWeights[cp].b1);
-					writeI16(&myFile, boneIndex);
-				}
-				if (bw.c > 1) {
-					boneIndex = OPfbxSkeletonGetPos(&mesh->Skeleton, mesh->BlendWeights[cp].b2);
-					writeI16(&myFile, boneIndex);
-				}
-				if (bw.c > 2) {
-					boneIndex = OPfbxSkeletonGetPos(&mesh->Skeleton, mesh->BlendWeights[cp].b3);
-					writeI16(&myFile, boneIndex);
-				}
-				if (bw.c > 3) {
-					boneIndex = OPfbxSkeletonGetPos(&mesh->Skeleton, mesh->BlendWeights[cp].b4);
-					writeI16(&myFile, boneIndex);
-				}
-
-				for (OPint k = 0; k < 4 - bw.c; k++) {
-					writeI16(&myFile, -1);
-				}
+		ui32 feature = 0;
+		if (features[Model_Positions]) feature += 0x01;
+		if (features[Model_Normals]) feature += 0x02;
+		if (features[Model_UVs]) feature += 0x04;
+		if (features[Model_Colors]) feature += 0x100;
+		if (features[Model_Indices]) feature += 0x10;
+		if (features[Model_Bones]) feature += 0x20;
+		if (features[Model_Skinning]) feature += 0x40;
+		if (features[Model_Animations]) feature += 0x80;
 
 
-				if (bw.c > 0) {
-					writeF32(&myFile, mesh->BlendWeights[cp].w1);
-				}
-				if (bw.c > 1) {
-					writeF32(&myFile, mesh->BlendWeights[cp].w2);
-				}
-				if (bw.c > 2) {
-					writeF32(&myFile, mesh->BlendWeights[cp].w3);
-				}
-				if (bw.c > 3) {
-					writeF32(&myFile, mesh->BlendWeights[cp].w4);
-				}
+		OPlog("Feature: %d", feature);
+		// OPM File Features
+		writeU32(&myFile, feature);
 
-				for (OPint k = 0; k < 4 - bw.c; k++) {
-					writeF32(&myFile, 0.0f);
-				}
-
-			}
+		OPint verticeCount = 0;
+		OPint indCount = 0;
+		for (OPint i = 0; i < mesh->MeshData.PolyCount; i++) {
+			verticeCount += mesh->Polys[i].Size;
+			indCount += mesh->Polys[i].Size == 3 ? 3 : 6;
 		}
-	}
 
-	OPint offset = 0;
-	if (features[Model_Indices]) {
-		writeU32(&myFile, indCount / 3);
+		OPlog("Vertex Count: %d", verticeCount);
+		// Vertex Count
+		writeU32(&myFile, verticeCount);
 
 		for (OPint i = 0; i < mesh->MeshData.PolyCount; i++) {
-			if (mesh->Polys[i].Size == 3) {
-				writeU16(&myFile, offset + 0);
-				writeU16(&myFile, offset + 1);
-				writeU16(&myFile, offset + 2);
-				offset += 3;
-			}
-			else {
-				writeU16(&myFile, offset + 0);
-				writeU16(&myFile, offset + 1);
-				writeU16(&myFile, offset + 2);
+			for (OPint j = 0; j < mesh->Polys[i].Size; j++) {
 
-				writeU16(&myFile, offset + 0);
-				writeU16(&myFile, offset + 2);
-				writeU16(&myFile, offset + 3);
-				offset += 4;
+				if (features[Model_Positions]) {
+					OPlg(".");
+					OPvec3 pos = mesh->Polys[i].Position[j];
+					writeF32(&myFile, pos.x);
+					writeF32(&myFile, pos.y);
+					writeF32(&myFile, pos.z);
+				}
+
+				if (features[Model_Normals]) {
+					OPlg("-");
+					OPvec3 norm = mesh->Polys[i].Normal[j];
+					writeF32(&myFile, norm.x);
+					writeF32(&myFile, norm.y);
+					writeF32(&myFile, norm.z);
+				}
+
+				if (features[Model_Tangents]) {
+					OPlg("%");
+					OPvec4 tan = mesh->Polys[i].Tangent[j];
+					writeF32(&myFile, tan.x);
+					writeF32(&myFile, tan.y);
+					writeF32(&myFile, tan.z);
+				}
+
+				if (features[Model_UVs]) {
+					OPlg("=");
+					OPvec2 uv = mesh->Polys[i].UV[j];
+					writeF32(&myFile, uv.x);
+					writeF32(&myFile, uv.y);
+				}
+
+				if (features[Model_Skinning]) {
+					OPlg("+");
+					OPint cp = mesh->Polys[i].ControlPointIndex[j];
+					OPfbxSkinBlendWeight bw = mesh->BlendWeights[cp];
+					OPint boneIndex;
+
+					if (bw.c > 0) {
+						boneIndex = OPfbxSkeletonGetPos(&mesh->Skeleton, mesh->BlendWeights[cp].b1);
+						writeI16(&myFile, boneIndex);
+					}
+					if (bw.c > 1) {
+						boneIndex = OPfbxSkeletonGetPos(&mesh->Skeleton, mesh->BlendWeights[cp].b2);
+						writeI16(&myFile, boneIndex);
+					}
+					if (bw.c > 2) {
+						boneIndex = OPfbxSkeletonGetPos(&mesh->Skeleton, mesh->BlendWeights[cp].b3);
+						writeI16(&myFile, boneIndex);
+					}
+					if (bw.c > 3) {
+						boneIndex = OPfbxSkeletonGetPos(&mesh->Skeleton, mesh->BlendWeights[cp].b4);
+						writeI16(&myFile, boneIndex);
+					}
+
+					for (OPint k = 0; k < 4 - bw.c; k++) {
+						writeI16(&myFile, -1);
+					}
+
+
+					if (bw.c > 0) {
+						writeF32(&myFile, mesh->BlendWeights[cp].w1);
+					}
+					if (bw.c > 1) {
+						writeF32(&myFile, mesh->BlendWeights[cp].w2);
+					}
+					if (bw.c > 2) {
+						writeF32(&myFile, mesh->BlendWeights[cp].w3);
+					}
+					if (bw.c > 3) {
+						writeF32(&myFile, mesh->BlendWeights[cp].w4);
+					}
+
+					for (OPint k = 0; k < 4 - bw.c; k++) {
+						writeF32(&myFile, 0.0f);
+					}
+
+				}
 			}
 		}
-	}
 
-	if (features[Model_Bones]) {
-		writeI16(&myFile, mesh->Skeleton.BoneCount);
-		for(OPint i = 0; i < mesh->Skeleton.BoneCount; i++) {
-			writeI16(&myFile, mesh->Skeleton.Bones[i].Parent); // TODO: Bone Parent
+		OPint offset = 0;
+		if (features[Model_Indices]) {
+			writeU32(&myFile, indCount / 3);
 
-			ui32 len = strlen(mesh->Skeleton.Bones[i].Name);
-			writeU32(&myFile, len);
-			write(&myFile, mesh->Skeleton.Bones[i].Name, len);
-			for (i32 c = 0; c < 4; c++) {
-				writeF32(&myFile, mesh->Skeleton.Bones[i].BindPose[c].x);
-				writeF32(&myFile, mesh->Skeleton.Bones[i].BindPose[c].y);
-				writeF32(&myFile, mesh->Skeleton.Bones[i].BindPose[c].z);
-				writeF32(&myFile, mesh->Skeleton.Bones[i].BindPose[c].w);
+			for (OPint i = 0; i < mesh->MeshData.PolyCount; i++) {
+				if (mesh->Polys[i].Size == 3) {
+					writeU16(&myFile, offset + 0);
+					writeU16(&myFile, offset + 1);
+					writeU16(&myFile, offset + 2);
+					offset += 3;
+				}
+				else {
+					writeU16(&myFile, offset + 0);
+					writeU16(&myFile, offset + 1);
+					writeU16(&myFile, offset + 2);
+
+					writeU16(&myFile, offset + 0);
+					writeU16(&myFile, offset + 2);
+					writeU16(&myFile, offset + 3);
+					offset += 4;
+				}
 			}
 		}
-		//WriteSkeleton(skel, &myFile);
+
+		if (features[Model_Bones]) {
+			writeI16(&myFile, mesh->Skeleton.BoneCount);
+			for (OPint i = 0; i < mesh->Skeleton.BoneCount; i++) {
+				writeI16(&myFile, mesh->Skeleton.Bones[i].Parent); // TODO: Bone Parent
+
+				ui32 len = strlen(mesh->Skeleton.Bones[i].Name);
+				writeU32(&myFile, len);
+				write(&myFile, mesh->Skeleton.Bones[i].Name, len);
+				for (i32 c = 0; c < 4; c++) {
+					writeF32(&myFile, mesh->Skeleton.Bones[i].BindPose[c].x);
+					writeF32(&myFile, mesh->Skeleton.Bones[i].BindPose[c].y);
+					writeF32(&myFile, mesh->Skeleton.Bones[i].BindPose[c].z);
+					writeF32(&myFile, mesh->Skeleton.Bones[i].BindPose[c].w);
+				}
+			}
+		}
 	}
 
 	if (features[Model_Animations]) {
