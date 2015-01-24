ee90f19b91b0fde7142b529548269e34eec26733
Garrett Hoofman <gambitsunob@gmail.com>
2015-01-24 13:19:25 -0500

    OPIFEX_ASSETS

--- CMake/engine/components.cmake
+++ CMake/engine/components.cmake
@@ -3,7 +3,8 @@ macro(add_opifex_assets APPLICATION_TARGET )
 	unset(OPIFEX_ASSETS)
 	
 	if(${OPIFEX_OPTION_RELEASE})
-
+		SET(OPIFEX_ASSETS NULL)
+		add_definitions(-DOPIFEX_ASSETS=NULL)
 	else()
 
 		add_definitions(-D_DEBUG)
