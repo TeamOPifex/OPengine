7ec5a141aebb2c400761234990a3349154c616c7
Garrett Hoofman <garrett@teamopifex.com>
2015-02-08 17:09:55 -0500

    No more VS2010 dependency
    Former-commit-id: 26a2ec473b0c8f541b4bc9754e6631725d43b7fd

--- CMake/engine/macros.cmake
+++ CMake/engine/macros.cmake
@@ -226,6 +226,28 @@ macro(copy_to_folder APPLICATION_TARGET RELATIVE_PATH FILE_PATH OUTPUT_PATH OPIF
 
 endmacro(copy_to_folder)
 
+
+
+macro(copy_to_folder_msvc APPLICATION_TARGET RELATIVE_PATH FILE_PATH OUTPUT_PATH OPIFEX_MATCH )
+
+	
+	if( ${OPIFEX_MATCH} )
+		populate_binary_directory()
+		if(${MSVC_VERSION} GREATER 1700)
+			add_custom_command(TARGET ${APPLICATION_TARGET} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different
+				"${PROJECT_SOURCE_DIR}${RELATIVE_PATH}${BINARY_TARGET_DIRECTORY}/vs2013/${FILE_PATH}"
+				${PROJECT_BINARY_DIR}${OUTPUT_PATH})
+		else()
+			add_custom_command(TARGET ${APPLICATION_TARGET} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different
+				"${PROJECT_SOURCE_DIR}${RELATIVE_PATH}${BINARY_TARGET_DIRECTORY}/${FILE_PATH}"
+				${PROJECT_BINARY_DIR}${OUTPUT_PATH})
+		endif()
+			
+	endif()
+
+endmacro(copy_to_folder_msvc)
+			
+
 macro(copy_from_binaries_on_build APPLICATION_TARGET FILE_PATH OPIFEX_MATCH )
 
 	if(${OPIFEX_OPTION_RELEASE})
