86306bffd143c4f4a641fe72b4b88916d70f1449
Garrett Hoofman <gambitsunob@gmail.com>
2015-04-05 07:55:07 -0400

    Some cleanup and the mesh builder

--- Human/include/Utilities/OPimagePNG.h
+++ Human/include/Utilities/OPimagePNG.h
@@ -8,12 +8,19 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+	struct OPimagePNG {
+		ui32 Width;
+		ui32 Height;
+		ui8* Data;
+	};
+
 	void OPimagePNG24WriteStream(ui8* imageData, i32 width, i32 height, ui8** data, OPuint* dataSize);
 	void OPimagePNG32WriteStream(ui8* imageData, i32 width, i32 height, ui8** data, OPuint* dataSize);
 	void OPimagePNGCreate24(ui8* imageData, i32 width, i32 height, OPchar* filename);
 	void OPimagePNGCreate32(ui8* imageData, i32 width, i32 height, OPchar* filename);
 	i32 OPimagePNGLoad(const OPchar* filename, OPtexture** image);
 	i32 OPimagePNGLoadStream(OPstream* str, i32 offset, OPtexture** image);
+	OPimagePNG OPimagePNGLoadData(const OPchar* filename);
 	i32 OPimagePNGReload(const OPchar* filename, OPtexture** image);
 	i32 OPimagePNGUnload(void* image);
 #ifdef __cplusplus
