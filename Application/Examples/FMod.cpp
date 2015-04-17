87c39d58384fbaa0c32e6a2d776456fcd72dd0d0
Garrett Hoofman <garrett@teamopifex.com>
2015-04-17 16:56:36 -0400

    Fmod single system init

--- Application/Examples/FMod.cpp
+++ Application/Examples/FMod.cpp
@@ -2,42 +2,41 @@
 #include "./Pipeline/include/OPfmod.h"
 
 typedef struct {
-	OPfmod fmod;
 	OPfmodSound sound[5];
 } FModExample;
 
 FModExample fmodExample;
 
 void ExampleFModEnter(OPgameState* last) {
-	OPfmodInit(&fmodExample.fmod);
-	OPfmodLoad(&fmodExample.fmod, &fmodExample.sound[0], "Audio/Pickup_Coin.wav");
-	OPfmodLoad(&fmodExample.fmod, &fmodExample.sound[1], "Audio/pew.wav");
-	OPfmodLoad(&fmodExample.fmod, &fmodExample.sound[2], "Audio/sfx.ogg");
-	OPfmodLoad(&fmodExample.fmod, &fmodExample.sound[3], "Audio/impact.wav");
-	OPfmodLoad(&fmodExample.fmod, &fmodExample.sound[4], "Audio/Powerup.wav");
+	OPfmodInit();
+	OPfmodLoad(&fmodExample.sound[0], "Audio/Pickup_Coin.wav");
+	OPfmodLoad(&fmodExample.sound[1], "Audio/pew.wav");
+	OPfmodLoad(&fmodExample.sound[2], "Audio/sfx.ogg");
+	OPfmodLoad(&fmodExample.sound[3], "Audio/impact.wav");
+	OPfmodLoad(&fmodExample.sound[4], "Audio/Powerup.wav");
 
 }
 
 OPint ExampleFModUpdate(OPtimer* time) {
-	OPfmodUpdate(&fmodExample.fmod);
+	OPfmodUpdate();
 	if (OPkeyboardWasPressed(OPKEY_L) || OPgamePadWasPressed(OPgamePadGet(OPGAMEPAD_ONE), OPGAMEPADBUTTON_A)) {
-		OPfmodPlay(&fmodExample.fmod, &fmodExample.sound[0]);
+		OPfmodPlay(&fmodExample.sound[0]);
 	}
 
 	if (OPkeyboardWasPressed(OPKEY_B) || OPgamePadWasPressed(OPgamePadGet(OPGAMEPAD_ONE), OPGAMEPADBUTTON_B)) {
-		OPfmodPlay(&fmodExample.fmod, &fmodExample.sound[1]);
+		OPfmodPlay(&fmodExample.sound[1]);
 	}
 
 	if (OPkeyboardWasPressed(OPKEY_C) || OPgamePadWasPressed(OPgamePadGet(OPGAMEPAD_ONE), OPGAMEPADBUTTON_B)) {
-		OPfmodPlay(&fmodExample.fmod, &fmodExample.sound[2]);
+		OPfmodPlay(&fmodExample.sound[2]);
 	}
 
 	if (OPkeyboardWasPressed(OPKEY_F) || OPgamePadWasPressed(OPgamePadGet(OPGAMEPAD_ONE), OPGAMEPADBUTTON_B)) {
-		OPfmodPlay(&fmodExample.fmod, &fmodExample.sound[3]);
+		OPfmodPlay(&fmodExample.sound[3]);
 	}
 
 	if (OPkeyboardWasPressed(OPKEY_A) || OPgamePadWasPressed(OPgamePadGet(OPGAMEPAD_ONE), OPGAMEPADBUTTON_B)) {
-		OPfmodPlay(&fmodExample.fmod, &fmodExample.sound[4]);
+		OPfmodPlay(&fmodExample.sound[4]);
 	}
 
 	OPrenderClear(0, 0, 0);
