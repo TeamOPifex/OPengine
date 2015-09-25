5c1c8fa31eb355a9a86c17c2e81293a72085de71
Garrett Hoofman <gambitsunob@gmail.com>
2015-09-25 19:23:27 -0400

    Changed up defines, removing OPengineDefines.h

---
+++ CMake/engine/macros/add_opifex_defines.cmake
@@ -0,0 +1,57 @@
+
+macro(add_opifex_defines)
+
+	if(${OPIFEX_OPTION_MYO})
+		add_definitions(-DOPIFEX_OPTION_MYO)
+	endif()
+
+	if(${OPIFEX_OPTION_OCULUS})
+		add_definitions(-DOPIFEX_OPTION_OCULUS)
+	endif()
+
+	if(${OPIFEX_OPTION_PHYSX})
+		add_definitions(-DOPIFEX_OPTION_PHYSX)
+	endif()
+
+	if(${OPIFEX_OPTION_RELEASE})
+		add_definitions(-DOPIFEX_OPTION_RELEASE)
+	endif()
+
+	if(${OPIFEX_OPTION_SHARED})
+		add_definitions(-DOPIFEX_OPTION_SHARED)
+	endif()
+
+	if(${OPIFEX_OPTION_SPINE})
+		add_definitions(-DOPIFEX_OPTION_SPINE)
+	endif()
+
+	if(${OPIFEX_OPTION_FMOD})
+		add_definitions(-DOPIFEX_OPTION_FMOD)
+	endif()
+
+	if(${OPIFEX_OPTION_AUDIO})
+		add_definitions(-DOPIFEX_OPTION_AUDIO)
+	endif()
+
+	if(${OPIFEX_OPTION_V8})
+		add_definitions(-DOPIFEX_OPTION_V8)
+	endif()
+
+	add_definitions(-DOPIFEX_${OPENGL_DESKTOP_TARGET})
+	add_definitions(-D${OPIFEX_OS})
+
+	if(${OPIFEX_OPTION_RELEASE})
+	else()
+		add_definitions(-D_DEBUG)
+	endif()
+
+	if(${OPIFEX_OPTION_NODEJS} OR ${OPIFEX_OPTION_V8})
+		add_definitions(-DOPIFEX_JAVASCRIPT)
+	endif()
+
+	if(${OPIFEX_ASSETS})
+	else()
+		add_definitions(-DOPIFEX_ASSETS=NULL)
+	endif()
+
+endmacro(add_opifex_defines)
