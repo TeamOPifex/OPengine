71e68c68a854ea86571fadf51d1990d16fd85816
DESKTOP-BB9KND1\ghoofman <ghoofman@ostusa.com>
2016-12-06 12:20:10 -0500

    OPstring implementation

--- Data/include/OPstring.h
+++ Data/include/OPstring.h
@@ -5,14 +5,53 @@
 OPint OPstringEquals(const OPchar* str, const OPchar* cmp);
 OPint OPstringStartsWith(OPchar* str, const OPchar* cmp);
 OPint OPstringEndsWith(OPchar* str, const OPchar* cmp);
-void OPstringRemoveFromStart(OPchar* str, i32 size);
-OPchar* OPstringGetNonConstant(const OPchar* str);
-OPchar* OPstringCreateMerged(const OPchar* str, const OPchar* add);
 OPchar* OPstringCopy(const OPchar* str);
-OPchar* OPstringSub(const OPchar* str, ui32 start, ui32 end);
-OPint OPstringContains(const OPchar* str, const OPchar* cmp);
+OPchar* OPstringCreateMerged(const OPchar* str, const OPchar* add);
 void OPstringToLower(OPchar* str);
-void OPstringScan(OPchar* destination, const OPchar* format, ...);
 i32 OPstringCount(OPchar* str, OPchar lookFor);
 i32 OPstringFirst(OPchar* str, OPchar lookFor);
 i32 OPstringSplit(const OPchar* str, OPchar split, OPchar*** output);
+void OPstringScan(OPchar* destination, const OPchar* format, ...);
+OPchar* OPstringGetNonConstant(const OPchar* str);
+OPchar* OPstringSub(const OPchar* str, ui32 start, ui32 end);
+OPint OPstringContains(const OPchar* str, const OPchar* cmp);
+
+void OPstringRemoveFromStart(OPchar* str, i32 size);
+
+
+struct OPstring {
+	OPchar* _data;
+	ui32 _len;
+
+	OPstring() { _data = NULL; _len = 0; }
+	OPstring(const OPchar* data);
+	~OPstring();
+
+	inline ui32 Length() { return _len; }
+	inline OPchar* C_Str() { return _data; }
+	
+	bool Equals(OPstring* str);
+	bool Equals(const OPchar* str);
+	bool StartsWith(OPstring* str);
+	bool StartsWith(const OPchar* str);
+	bool EndsWith(OPstring* str);
+	bool EndsWith(const OPchar* str);
+	OPstring* Copy();
+	void Add(OPstring* str);
+	void Add(const OPchar* str);
+	OPstring* Substr(OPuint start, OPuint end);
+	OPint Contains(OPstring* str);
+	OPint Contains(const OPchar* str);
+	void ToLower();
+	OPint IndexOf(OPchar c);
+	OPint IndexOfLast(OPchar c);
+	OPuint Count(OPchar c);
+	i32 Split(OPchar split, OPchar*** output);
+	void Resize(OPuint size, bool realloc);
+	inline void Resize(OPuint size) { Resize(size, false); }
+
+	void Init(OPchar* data);
+	void Init(const OPchar* data);
+
+	static OPstring* Create(const OPchar* data);
+};
